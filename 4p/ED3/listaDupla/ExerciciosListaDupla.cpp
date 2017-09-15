7eb5b3ab76e68a59f6cf3b0c4202b7eccb89a1ce
vivianavfs <vivianavfs@yahoo.com>
2017-09-15 20:16:54 -0300

    Update ExerciciosListaDupla.cpp

--- 4p/ED3/listaDupla/ExerciciosListaDupla.cpp
+++ 4p/ED3/listaDupla/ExerciciosListaDupla.cpp
@@ -70,6 +70,51 @@ celula *encontra(int v, lista l)
 	else return NULL;
 }
 
+lista inserirOrdenado(lista l, celula *nova)
+{
+	if(l.inicio==NULL){
+		l.inicio = nova;
+		l.fim = nova;
+		return l;
+	}else{
+		celula *aux = l.inicio;
+		while(aux && aux->elem < nova->elem)
+		{
+			cout << "E: " << aux->elem << endl;
+			aux = aux->prox;
+		}
+		if(aux==l.inicio){
+			nova->prox = aux;
+			aux->ant = nova;
+			l.inicio = nova;
+		}
+		else if(aux==NULL)
+		{
+			l.fim->prox = nova;
+			nova->ant = l.fim;
+			l.fim = nova;
+		}
+		else{
+			nova->ant = aux->ant;
+			nova->prox = aux;
+			aux->ant->prox = nova;
+		}
+		return l;
+	}
+}
+
+void imprimir(lista l) {
+if (l.inicio) {
+celula *aux = l.inicio;
+while (aux) {
+cout << aux->elem << " ";
+aux = aux->prox;
+}
+}
+else cout << "Lista vazia";
+cout << endl;
+}
+
 int main()
 {
 	lista l;
@@ -77,22 +122,27 @@ int main()
 	l.fim = NULL;
 
 	l = inserirInicio(10, l);
-	l = inserirInicio(2, l);
+	l = inserirInicio(9, l);
+	l = inserirInicio(8, l);
 	l = inserirInicio(3, l);
-	l = inserirInicio(7, l);
 
 	//exercicio 01
 	imprimirInvertido(l);
 	
 	//exercicio 02
-	celula *valor = encontra(8,l);
+	celula *valor = encontra(3,l);
 	if(valor)
 		cout << valor->elem << " - " << valor << endl;
 	else
 		cout << "NULL" << endl;
 	
 	//exercicio 03
+	celula *nova = new celula;
+	nova->elem = 1;
+	nova->ant = NULL;
+	nova->prox = NULL;
+	l = inserirOrdenado(l,nova);
 	
-
+	imprimir(l);
 	return 0;
 }
